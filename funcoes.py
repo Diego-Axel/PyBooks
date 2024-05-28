d8782a06dbadee297862b245fa639e9c93d2c17e
Diego-Axel <diegoaxelbsr@gmail.com>
2024-05-28 16:26:07 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -775,9 +775,9 @@ def exibir_venda():
     os.system('clear || cls') # se for Linux use 'clear' e se for Windowns use 'cls'
     print()
     print("############################################")
-    print("#####      Exibir Dados do Vendas      #####")
+    print("#####      Exibir Dados de Vendas      #####")
     print("############################################")
-    print("##### 0 - Retornar ao Menu Vendas     #####")
+    print("##### 0 - Retornar ao Menu Vendas      #####")
     print("############################################")
     print()
     verificador = True
@@ -789,13 +789,19 @@ def exibir_venda():
         print("!!!! Resposta não reconhecida como um número INTEIRO. Tente novamente.")
     if (code_venda in vendas) and (vendas[code_venda][6]):
       print()
-      print("##### Data de venda: ",vendas[code_venda][0])
-      print("##### Nome do Cliente: ",vendas[code_venda][1])
-      print("##### Livro Comprado: ",vendas[code_venda][2])
-      print("##### Unidades Vendias: ",vendas[code_venda][3])
-      print("##### Valor R$ da Venda",vendas[code_venda][4])
-      print("##### Forma de Pagamento: ",vendas[code_venda][5])
-      print()
+      print("#####################################################################################################################################")
+      print("##########################################               Dados da Venda              ################################################")
+      print("#####################################################################################################################################")
+      print("|------------|---------------------------------------------|---------------------------------------------|----|--------|------------|")
+      print("| Data Venda |        Nome do Cliente/Comprador            |                 Livro Comprado              | UN |   R$   | Forma Pgt. |")
+      print("|------------|---------------------------------------------|---------------------------------------------|----|--------|------------|")
+      print("| %-10s "%(vendas[code_venda][0]), end="") # 1
+      print("| %-43s "%(vendas[code_venda][1]), end="") # 2
+      print("| %-43s "%(vendas[code_venda][2]), end="") # 3
+      print("| %-2s "%(vendas[code_venda][3]), end="")  # 4
+      print("| %-6s "%(vendas[code_venda][4]), end="")  # 5
+      print("| %-10s "%(vendas[code_venda][5]))         # 6
+      print("-------------------------------------------------------------------------------------------------------------------------------------")
     else:
       print()
       print("Venda inexistente ou deletada!")
@@ -823,25 +829,27 @@ def alterar_venda():
     os.system('clear || cls') # se for Linux use 'clear' e se for Windowns use 'cls'
     if (code_venda in vendas) and (vendas[code_venda][6]):
       print()
-      print("###########################################")
-      print("######      Dados Atuais da Venda     #####")
-      print("###########################################")
-      print("##### 0 - Cancelar e Retornar         #####")
-      print("###########################################")
-      print()
-      print("##### Data de venda: ",vendas[code_venda][0])
-      print("##### Nome do Cliente: ",vendas[code_venda][1])
-      print("##### Livro Comprado: ",vendas[code_venda][2])
-      print("##### Unidades Vendias: ",vendas[code_venda][3])
-      print("##### Valor R$ de Venda",vendas[code_venda][4])
-      print("##### Forma de Pagamento: ",vendas[code_venda][5])
+      print("#####################################################################################################################################")
+      print("##########################################               Dados da Venda              ################################################")
+      print("##########################################              0 - Para Cancelar            ################################################")
+      print("#####################################################################################################################################")
+      print("|------------|---------------------------------------------|---------------------------------------------|----|--------|------------|")
+      print("| Data Venda |        Nome do Cliente/Comprador            |                 Livro Comprado              | UN |   R$   | Forma Pgt. |")
+      print("|------------|---------------------------------------------|---------------------------------------------|----|--------|------------|")
+      print("| %-10s "%(vendas[code_venda][0]), end="") # 1
+      print("| %-43s "%(vendas[code_venda][1]), end="") # 2
+      print("| %-43s "%(vendas[code_venda][2]), end="") # 3
+      print("| %-2s "%(vendas[code_venda][3]), end="")  # 4
+      print("| %-6s "%(vendas[code_venda][4]), end="")  # 5
+      print("| %-10s "%(vendas[code_venda][5]))         # 6
+      print("-------------------------------------------------------------------------------------------------------------------------------------")
       print()
       resp = input("#### Qual dado deseja alterar? ")
       resp = resp.upper()
       print()
       ativo_venda = True
       vendas[code_venda][6] = ativo_venda
-      if (resp == "DATA DE VENDA") or (resp == "DATA"):
+      if (resp == "DATA DE VENDA") or (resp == "DATA") or (resp == "DATA VENDA"):
         data_venda = ("%02d/%02d/%d"%(dia, mes, ano))
         print("Data modificada para o dia de hoje!")
         print("-----------------------------------")
@@ -855,7 +863,7 @@ def alterar_venda():
         else:
           print()
           verificador = True
-      elif (resp == "NOME DO CLIENTE") or (resp == "NOME CLIENTE") or (resp == "CLIENTE"):    
+      elif (resp == "NOME DO CLIENTE") or (resp == "NOME CLIENTE") or (resp == "CLIENTE") or (resp == "COMPRADOR") or (resp == "NOME DO COMPRADOR"):    
         nome_cliente_venda = input("##### Digite o nome do Cliente: ")
         print("Dado alterado com sucesso!")
         print("--------------------------")
@@ -883,7 +891,7 @@ def alterar_venda():
         else:
           print()
           verificador = True
-      elif (resp == "UNIDADES") or (resp == "UNIDADES ADQUIRIDAS") or (resp == "UNIDADES VENDIDAS"):
+      elif (resp == "UNIDADES") or (resp == "UNIDADES ADQUIRIDAS") or (resp == "UNIDADES VENDIDAS") or (resp == "UN"):
         verificador = True
         while verificador:
           try:
@@ -922,7 +930,7 @@ def alterar_venda():
         else:
           print()
           verificador = True
-      elif (resp == "FORMA DE PAGAMENTO") or (resp == "PAGAMENTO"):
+      elif (resp == "FORMA DE PAGAMENTO") or (resp == "PAGAMENTO") or (resp == "Forma Pgt") or (resp == "Forma Pgt."):
         forma_pgto = input("##### Digite a Forma de pagamento: ")
         print("Dado alterado com sucesso!")
         print("--------------------------")
@@ -1136,7 +1144,7 @@ def relatorio_vendas():
     resp = input("##### Escolha sua opção: ")
     if resp == "1":
       os.system('clear || cls') # se for Linux use 'clear' e se for Windowns use 'cls'
-      print()
+      print() 
       print("#############################################################################################################################")
       print("#####################################              Relatório de Vendas          #############################################")
       print("#############################################################################################################################")

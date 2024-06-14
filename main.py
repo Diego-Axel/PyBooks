947cd440d1e93be226b11e262bc7234efb10f52e
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-13 21:47:12 -0300

    código refatorado

--- main.py
+++ main.py
@@ -6,8 +6,10 @@
 ############################################################
 
 # imports
-import funcoes # Arquivo com as minhas funções (modularização)
 import interfaces # Arquvio com as minhas interfaces (modularização)
+import fun_cliente # Arquivo com as minhas funções de Clientes (modularização)
+import fun_estoque # Arquivo com as minhas funções do Estoque (modularização)
+import fun_vendas # Arquivo com as minhas funções  de Vendas (modularização)
 import os # função para limpar a tela do terminal
 
 ################################################################
@@ -18,79 +20,79 @@ import os # função para limpar a tela do terminal
 
 op_princ = ""
 while op_princ != "0":
-  op_princ = funcoes.menu_principal()
+  op_princ = interfaces.menu_principal()
   print()
   if op_princ == "1":
     op_cliente = ""
     while op_cliente != "0":
-      op_cliente = funcoes.menu_cliente()
+      op_cliente = interfaces.menu_cliente()
       print()
       if op_cliente == "1":
-        funcoes.cadastrar_cliente()
+        fun_cliente.cadastrar_cliente()
       elif op_cliente == "2":
-        funcoes.exibir_cliente()
+        fun_cliente.exibir_cliente()
       elif op_cliente == "3":
-        funcoes.alterar_cliente()
+        fun_cliente.alterar_cliente()
       elif op_cliente == "4":
-        funcoes.excluir_cliente()
+        fun_cliente.excluir_cliente()
   elif op_princ == "2":
     op_estoque = ""
     while op_estoque != "0":
-      op_estoque = funcoes.menu_estoque()
+      op_estoque = interfaces.menu_estoque()
       print()
       if op_estoque == "1":
-        funcoes.cadastrar_produto()
+        fun_estoque.cadastrar_produto()
       elif op_estoque == "2":
-        funcoes.exibir_produto()
+        fun_estoque.exibir_produto()
       elif op_estoque == "3":
-        funcoes.alterar_produto()
+        fun_estoque.alterar_produto()
       elif op_estoque == "4":
-        funcoes.excluir_produto()
+        fun_estoque.excluir_produto()
   elif op_princ == "3":
     op_vendas = ""
     while op_vendas != "0":
-      op_vendas = funcoes.menu_vendas()
+      op_vendas = interfaces.menu_vendas()
       print()
       if op_vendas == "1":
-        funcoes.cadastrar_venda()
+        fun_vendas.cadastrar_venda()
       elif op_vendas == "2":
-        funcoes.exibir_venda()
+        fun_vendas.exibir_venda()
       elif op_vendas == "3":
-        funcoes.alterar_venda()
+        fun_vendas.alterar_venda()
       elif op_vendas == "4":
-        funcoes.excluir_venda()
+        fun_vendas.excluir_venda()
   elif op_princ == "4":
     op_relatorio = ""
     while op_relatorio != "0":
-      op_relatorio = funcoes.menu_relatorio()
+      op_relatorio = interfaces.menu_relatorio()
       print()
       if op_relatorio == "1":
         op_relatorio_clientes = ""
         while op_relatorio_clientes != "0":
-          op_relatorio_clientes = funcoes.menu_relatorio_clientes()
+          op_relatorio_clientes = interfaces.menu_relatorio_clientes()
           print()
           if op_relatorio_clientes == "1":
-            funcoes.relatorio_clientes_on()
+            fun_cliente.relatorio_clientes_on()
           elif op_relatorio_clientes == "2":
-            funcoes.relatorio_clientes_off()
+            fun_cliente.relatorio_clientes_off()
       elif op_relatorio == "2":
         op_relatorio_estoque = ""
         while op_relatorio_estoque != "0":
-          op_relatorio_estoque = funcoes.menu_relatorio_estoque()
+          op_relatorio_estoque = interfaces.menu_relatorio_estoque()
           print()
           if op_relatorio_estoque == "1":
-            funcoes.relatorio_estoque_on()
+            fun_estoque.relatorio_estoque_on()
           elif op_relatorio_estoque == "2":
-            funcoes.relatorio_estoque_off()
+            fun_estoque.relatorio_estoque_off()
       elif op_relatorio == "3":
         op_relatorio_vendas = ""
         while op_relatorio_vendas != "0":
-          op_relatorio_vendas = funcoes.menu_relatorio_vendas()
+          op_relatorio_vendas = interfaces.menu_relatorio_vendas()
           print()
           if op_relatorio_vendas == "1":
-            funcoes.relatorio_vendas_on()
+            fun_vendas.relatorio_vendas_on()
           elif op_relatorio_vendas == "2":
-            funcoes.relatorio_vendas_off()
+            fun_vendas.relatorio_vendas_off()
   elif op_princ == "5":
     interfaces.menu_informacao()
 
@@ -99,4 +101,6 @@ print()
 print("Programa encerrado. Até breve...")
 print()
 interfaces.encerramento() 
-funcoes.salvar_dados() # Função para guardar os dados de CLIENTES, PRODUTOS e VENDAS
+fun_cliente.salvar_dados_clientes() # Função para guardar os dados de CLIENTES,
+fun_estoque.salvar_dados_produtos() # Função para guardar os dados de PRODUTOS,
+fun_vendas.salvar_dados_vendas() # Função para guardar os dados de VENDAS

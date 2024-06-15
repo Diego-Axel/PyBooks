44e7fa728ec5064a35a7b02b3c89275401add687
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-14 22:00:50 -0300

    Update validadores.py

--- validadores.py
+++ validadores.py
@@ -1,1 +1,21 @@
 '''Validações'''
+
+'''imports'''
+import re
+
+#################################################
+#####            VALIDAR -EMAIL             #####
+#################################################
+
+def validar_email(email): # Função para verificar se o e-mail é válido (GPT)
+    padrao = re.compile(r'^[\w-]+@[a-z\d]+\.[\w]{2,3}$')
+    return padrao.match(email) is not None
+
+
+#################################################
+#####            VALIDAR CELULAR            #####
+#################################################
+
+def validar_numero(numero): # Função para validar telefone (GPT)
+    padrao = re.compile(r'^\(\d{2}\) \d{4,5}-\d{4}$')
+    return padrao.match(numero) is not None

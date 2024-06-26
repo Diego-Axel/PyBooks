c84ed3f8a8cde1b5e6833c7aaee44444e8cb4c4f
Diego-Axel <diegoaxelbsr@gmail.com>
2024-06-25 21:11:23 -0300

    Update validadores.py

--- validadores.py
+++ validadores.py
@@ -1,3 +1,4 @@
+
 '''Validações'''
 
 '''imports'''
@@ -20,55 +21,3 @@ def validar_email(email): # Função para verificar se o e-mail é válido (GPT)
 def validar_numero(numero): # Função para validar telefone (GPT)
     padrao = re.compile(r'^\(\d{2}\) \d{4,5}-\d{4}$')
     return padrao.match(numero) is not None
-
-
-#################################################
-#####              VALIDAR DATA             #####
-#################################################
-
-data = datetime.date.today()
-
-dia = data.day
-mes = data.month
-ano = data.year
-
-def validar_data(data_final):
-    verificador = True
-    while verificador:
-        try:
-            dia_venda = int(input("Insira o dia da venda: "))
-            if (dia_venda <= 31):
-                verificador = False
-            else:
-                print("OPS! Aparentemente você colocou um dia inválido. Tente novamante.")
-                print()
-        except ValueError:
-            print("Coloque um dia válido ou certifique-se que está inserindo um número inteiro. Por favor, digite novamente.")
-            print()
-    verificador = True
-    while verificador:
-        try:
-            mes_venda = int(input("Insira o mês da venda: "))
-            if (mes_venda <= 12):
-                verificador = False
-            else:
-                print("OPS! Aparentemente você colocou um mês inválido. Tente novamante.")
-                print()
-        except ValueError:
-            print("Coloque um mês válido ou certifique-se que está inserindo um número inteiro. Por favor, digite novamente.")
-            print()
-    verificador = True
-    while verificador:
-        try:
-            ano_venda = int(input("Insira o ano da venda: "))
-            if (ano_venda <= ano):
-                verificador = False
-            else:
-                print("OPS! Aparentemente você colocou um ano inválido. Tente novamante.")
-                print()
-        except ValueError:
-            print("Coloque um ano válido ou certifique-se que está inserindo um número inteiro. Por favor, digite novamente.")
-            print()
-    print()
-    data_final = ("%02d/%02d/%d"%(dia_venda,mes_venda,ano_venda))
-    return data_final

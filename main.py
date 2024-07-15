553e1f1dfc979c8a9efbaa5eaa6700b75bf3b9aa
Diego-Axel <diegoaxelbsr@gmail.com>
2024-07-15 19:56:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,45 @@
 ######                    CAICÓ-RN                    ######
 ############################################################
 
-# imports
-import interfaces # Arquvio com as minhas interfaces (modularização)
-import fun_cliente # Arquivo com as minhas funções de Clientes (modularização)
-import fun_estoque # Arquivo com as minhas funções do Estoque (modularização)
-import fun_vendas # Arquivo com as minhas funções  de Vendas (modularização)
+# imports na main
 import os
+import interfaces # Arquvio com as minhas interfaces no menu principal
+
+# imports clientes
+import clientes.interfaces as interface_cl
+import clientes.cadastrar as cadastrar_cl
+import clientes.exibir as exibir_cl
+import clientes.alterar as alterar_cl
+import clientes.excluir as excluir_cl
+import clientes.relatorio as relatorio_cl
+import clientes.salvar as salvar_cl
+
+# Imports estoque
+import estoque.interfaces as interface_es
+import estoque.cadastrar as cadastrar_es
+import estoque.exibir as exibir_es
+import estoque.alterar as alterar_es
+import estoque.excluir as excluir_es
+import estoque.relatorio as relatorio_es
+import estoque.salvar as salvar_es
+
+# imports vendas
+import vendas.interfaces as interface_vd
+import vendas.cadastrar as cadastrar_vd
+import vendas.exibir as exibir_vd
+import vendas.alterar as alterar_vd
+import vendas.excluir as excluir_vd
+import vendas.relatorio as relatorio_vd
+import vendas.salvar as salvar_vd
+
+
+# imports interfaces de relatórios
+import relatorios.interfaces as interface_rel
+
+
 
 ################################################################
-################################################################
 ##########            PROGRAMA PRINCIPAL              ##########
-################################################################
 ################################################################
 
 op_princ = ""
@@ -25,74 +53,74 @@ while op_princ != "0":
   if op_princ == "1":
     op_cliente = ""
     while op_cliente != "0":
-      op_cliente = interfaces.menu_cliente()
+      op_cliente = interface_cl.menu_cliente()
       print()
       if op_cliente == "1":
-        fun_cliente.cadastrar_cliente()
+        cadastrar_cl.cadastrar_cliente()
       elif op_cliente == "2":
-        fun_cliente.exibir_cliente()
+        exibir_cl.exibir_cliente()
       elif op_cliente == "3":
-        fun_cliente.alterar_cliente()
+        alterar_cl.alterar_cliente()
       elif op_cliente == "4":
-        fun_cliente.excluir_cliente()
+        excluir_cl.excluir_cliente()
   elif op_princ == "2":
     op_estoque = ""
     while op_estoque != "0":
-      op_estoque = interfaces.menu_estoque()
+      op_estoque = interface_es.menu_estoque()
       print()
       if op_estoque == "1":
-        fun_estoque.cadastrar_produto()
+        cadastrar_es.cadastrar_produto()
       elif op_estoque == "2":
-        fun_estoque.exibir_produto()
+        exibir_es.exibir_produto()
       elif op_estoque == "3":
-        fun_estoque.alterar_produto()
+        alterar_es.alterar_produto()
       elif op_estoque == "4":
-        fun_estoque.excluir_produto()
+        excluir_es.excluir_produto()
   elif op_princ == "3":
     op_vendas = ""
     while op_vendas != "0":
-      op_vendas = interfaces.menu_vendas()
+      op_vendas = interface_vd.menu_vendas()
       print()
       if op_vendas == "1":
-        fun_vendas.cadastrar_venda()
+        cadastrar_vd.cadastrar_venda()
       elif op_vendas == "2":
-        fun_vendas.exibir_venda()
+        exibir_vd.exibir_venda()
       elif op_vendas == "3":
-        fun_vendas.alterar_venda()
+        alterar_vd.alterar_venda()
       elif op_vendas == "4":
-        fun_vendas.excluir_venda()
+        excluir_vd.excluir_venda()
   elif op_princ == "4":
     op_relatorio = ""
     while op_relatorio != "0":
-      op_relatorio = interfaces.menu_relatorio()
+      op_relatorio = interface_rel.menu_relatorio()
       print()
       if op_relatorio == "1":
         op_relatorio_clientes = ""
         while op_relatorio_clientes != "0":
-          op_relatorio_clientes = interfaces.menu_relatorio_clientes()
+          op_relatorio_clientes = interface_rel.menu_relatorio_clientes()
           print()
           if op_relatorio_clientes == "1":
-            fun_cliente.relatorio_clientes_on()
+            relatorio_cl.relatorio_clientes_on()
           elif op_relatorio_clientes == "2":
-            fun_cliente.relatorio_clientes_off()
+            relatorio_cl.relatorio_clientes_off()
       elif op_relatorio == "2":
         op_relatorio_estoque = ""
         while op_relatorio_estoque != "0":
-          op_relatorio_estoque = interfaces.menu_relatorio_estoque()
+          op_relatorio_estoque = interface_rel.menu_relatorio_estoque()
           print()
           if op_relatorio_estoque == "1":
-            fun_estoque.relatorio_estoque_on()
+            relatorio_es.relatorio_estoque_on()
           elif op_relatorio_estoque == "2":
-            fun_estoque.relatorio_estoque_off()
+            relatorio_es.relatorio_estoque_off()
       elif op_relatorio == "3":
         op_relatorio_vendas = ""
         while op_relatorio_vendas != "0":
-          op_relatorio_vendas = interfaces.menu_relatorio_vendas()
+          op_relatorio_vendas = interface_rel.menu_relatorio_vendas()
           print()
           if op_relatorio_vendas == "1":
-            fun_vendas.relatorio_vendas_on()
+            relatorio_vd.relatorio_vendas_on()
           elif op_relatorio_vendas == "2":
-            fun_vendas.relatorio_vendas_off()
+            relatorio_vd.relatorio_vendas_off()
   elif op_princ == "5":
     interfaces.menu_informacao()
 
@@ -101,6 +129,6 @@ print()
 print("Programa encerrado. Até breve...")
 print()
 interfaces.encerramento() 
-fun_cliente.salvar_dados_clientes() # Função para guardar os dados de CLIENTES,
-fun_estoque.salvar_dados_produtos() # Função para guardar os dados de PRODUTOS,
-fun_vendas.salvar_dados_vendas() # Função para guardar os dados de VENDAS
+salvar_cl.salvar_dados_clientes() # Função para guardar os dados de CLIENTES,
+salvar_es.salvar_dados_produtos() # Função para guardar os dados de PRODUTOS,
+salvar_vd.salvar_dados_vendas() # Função para guardar os dados de VENDAS

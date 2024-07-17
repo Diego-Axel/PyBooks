95ec6f3876e9dbb386753528caac4787fa1514e5
Diego-Axel <diegoaxelbsr@gmail.com>
2024-07-17 15:14:48 -0300

    Update validadores.py

--- validadores.py
+++ validadores.py
@@ -1,7 +1,7 @@
 
 '''Validações'''
 
-import datetime # Para ver se a data inserida é valida.
+import datetime 
 data = datetime.date.today()
 dia = data.day
 mes = data.month
@@ -80,5 +80,29 @@ def validar_data(): # Função para validar a data (feita por mim)
 #####             VALIDAR CPF               #####
 #################################################
 
-def validar_cpf(cpf): # Vou colocar ainda
-    pass
+def validar_cpf(cpf): # Disponibilizada pelo professor Flavius 
+    tam = len(cpf)
+    soma = 0
+    d1 = 0
+    d2 = 0
+    if tam != 11:
+        return False
+    for i in range(11):
+        if (cpf[i]<'0')or(cpf[i]>'9'):
+            return False
+    for i in range(9):
+        soma += (int(cpf[i])*(10 - i))
+    d1 = 11 - (soma % 11)
+    if (d1 == 10 or d1 == 11):
+        d1 = 0
+    if d1 != int(cpf[9]):
+        return False
+    soma = 0
+    for i in range(10):
+        soma += (int(cpf[i])*(11-i))
+    d2 = 11 - (soma%11)
+    if (d2 == 10 or d2 == 11):
+        d2 = 0
+    if d2 != int(cpf[10]):
+        return False
+    return True
